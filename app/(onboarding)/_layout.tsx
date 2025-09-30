34fd3c7b662a2b4bb99e2ad3ebf02e5be1ed1cf3
baka <sak24835@gmail.com>
2025-09-30 09:10:08 +0530

    completed login

--- app/(onboarding)/_layout.tsx
+++ app/(onboarding)/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from 'expo-router';
 
 export default function OnboardingLayout() {
   return (
+
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="splash" />
       <Stack.Screen name="language-selection" />
